05275a5dc1a1aa22613a8e8f85e627bafe768b2b
Yang Liu <yliu0@cs.washington.edu>
2020-09-26 21:20:42 -0700

    Integrated A6, A9, A13, A14, A19 for H1

---
+++ src/boba_util.R
@@ -0,0 +1,204 @@
+# check if we support the model type
+# @param model The fitted model object
+is_supported <- function (model) {
+  ms <- c('lm', 'lmerMod', 'negbin', 'aov')
+  return(class(model)[1] %in% ms)
+}
+
+# get model predictions per data point
+# @param model The fitted model object
+# @param df The dataframe that the model will predict on
+pointwise_predict <- function (model, df) {
+  if (!is_supported(model)) {
+    stop(paste('Unsupported model type', class(model)[1]))
+  }
+
+  # fixme: lmerMod does not have se.fit
+  pred <- predict(model, df, se.fit = TRUE, type = "response")
+
+  disagg_fit <- df  %>%
+    mutate(
+      fit = pred$fit,                                     # inferential fits
+      se.fit = pred$se.fit,                               # standard errors of predicted means
+      df = df.residual(model),                            # residual degrees of freedom
+      sigma = sigma(model),                               # residual standard deviation
+      se.residual = sqrt(sum(residuals(model)^2) / df)    # residual standard errors
+    ) %>%
+    # remove rows where model can't make a prediction
+    drop_na(fit)
+  return(disagg_fit)
+}
+
+# split the train/test set in a k-fold cross validation
+# returns a dataframe with k rows (k is the num of folds) and two columns
+#   - train: a list of training indices for the k-th fold
+#   - test: a list of testing indices for the k-th fold
+# @param n The total number of rows
+cv_split <- function (n, folds = 5) {
+  l = n %/% folds
+  rest = n - folds * l
+  
+  lengths <- ifelse(1:folds <= rest, l + 1, l)
+  f_sum <- function(x, n) sum(head(x,n))
+  indices <- lapply(1:folds, function (i) {
+    i1 = f_sum(lengths, i - 1) + 1
+    i2 = i1 + lengths[i] - 1
+    
+    if (i1 > 1) {
+      if (i2+1 < n) {
+        i_train = c(1:(i1-1), (i2+1):n)
+      } else {
+        i_train = 1:(i1-1)
+      }
+    } else {
+      i_train = (i2+1):n
+    }
+    i_test = c(i1:i2)
+    return(list(i_train, i_test))
+  })
+  
+  indices <- as.data.frame(do.call(rbind, indices))
+  colnames(indices) <- c("train", "test")
+  return(indices)
+}
+
+# perform k-fold cross validation
+# @param df The dataframe
+# @param model The fitted model
+# @param y The column name for the observed variable in df
+# @param folds The number of folds
+# @param func A function returning the fitted y vector from a model and a dataset
+cross_validation <- function (df, model, y, folds = 5, func = NULL) {
+  mse = 0
+  indices = cv_split(nrow(df), folds = folds)
+  for (i in c(1:nrow(indices))) {
+    d_train = df[indices$train[[i]], ]
+    d_test = df[indices$test[[i]], ]
+
+    m1 <- update(model, . ~ ., data = d_train)
+    if (!is.null(func)) {
+        expected <- func(m1, d_test)
+    } else {
+        # fixme: lmerMod need to set allow.new.levels = TRUE
+        expected <- pointwise_predict(m1, d_test)$fit
+    }
+
+    mse = mse + sum((d_test[[y]] - expected)^2)
+  }
+
+  mse = sqrt(mse / nrow(df))
+  return(mse)
+}
+
+# marginalize model predictions
+# @param df The dataframe containing individual model fits
+# @param term The predictor of interest
+# @param y The value field to aggregate
+margins <- function (df, term, y = "fit") {
+  expectation <- df %>%
+    group_by(!! sym(term)) %>%                   # group by predictor(s) of interest
+    summarize(expected = weighted.mean(!! sym(y))) %>%  # marninalize across other predictors
+    compare_levels(expected, by = !! sym(term)) %>%
+    ungroup()
+  return(expectation)
+}
+
+# get the sampling distribution
+# @param model The fitted model
+# @param term The predictor of interest
+# @param type Type of result (response or model coefficient)
+# @param draws The number of draws
+sampling_distribution <- function (model, term, type="coef", draws=200) {
+  if (!is_supported(model)) {
+    stop(paste('Unsupported model type', class(model)[1]))
+  }
+  ts = c('coef', 'coefficient', 'resp', 'response')
+  if (!(type %in% ts)) {
+    stop(paste('Unsupported type', type))
+  }
+
+  if (type == "coef" || type == "coefficient") {
+    uncertainty <- tidy(model, conf.int = TRUE) %>%
+      filter(term == !! term) %>%
+      mutate(
+        df = df.residual(model),                # get model degrees of freedom
+        .draw = list(1:draws),                  # generate list of draw numbers
+        t = map(df, ~rt(draws, .))              # simulate draws as t-scores
+      ) %>%
+      unnest(cols = c(".draw", "t")) %>%
+      mutate(coef = t * std.error + estimate)
+  }
+
+  if (type == "resp" || type == "response") {
+    # todo
+  }
+
+  return(uncertainty)
+}
+
+# permutation test to get the null distribution
+# @param df The dataframe
+# @param model The fitted model
+# @param terms A character vector of terms to be shuffled
+# @param func A function returning the point estimate from a model and a dataset
+# @param N The number of iterations
+permutation_test <- function (df, model, terms, func = NULL, N=200) {
+  # ensure we have the same random samples across universe runs
+  set.seed(3040)
+
+  res = lapply(1:N, function (i) {
+    # shuffle
+    pm <- df[sample(nrow(df)), ] %>%
+      dplyr::select(any_of(terms))
+
+    df2 = df %>% dplyr::select(-any_of(terms)) %>%
+      bind_cols(pm)
+
+    # fit the model
+    m1 <- update(model, . ~ ., data = df2)
+
+    # point estimate
+    if (!is.null(func)) {
+      expected <- func(m1, df2)
+    } else {
+      # fixme: lmerMod need to set allow.new.levels = TRUE
+      expected <- margins(pointwise_predict(m1, df2), terms[1])$expected
+    }
+
+    return(expected)
+  })
+
+  # remove seed because set seed is global
+  rm(.Random.seed, envir=.GlobalEnv)
+
+  return(enframe(unlist(res)))
+}
+
+# get the pointwise log likelihood
+# @param model The fitted model
+# @param d_test The dataframe
+# @private
+compute_loglik <- function (model, d_test) {
+  mu <- predict(model, d_test, type = "response")
+  sigma <- sigma(model)
+  y <- as.list(attr(terms(model), "variables"))[[2]]
+  return(log(dnorm(d_test[[y]], mu, sigma)+1e-307))
+}
+
+# get the pointwise log likelihood for stacking
+# @param df The dataframe
+# @param model The fitted model
+stacking <- function (df, model) {
+  indices = cv_split(nrow(df), folds = 5)
+  pointwise_density <- c()
+
+  for (i in c(1:nrow(indices))) {
+    d_train = df[indices$train[[i]], ]
+    d_test = df[indices$test[[i]], ]
+
+    m1 <- update(model, . ~ ., data = d_train)
+    pointwise_density <- append(pointwise_density, compute_loglik(m1, d_test))
+  }
+
+  return(pointwise_density)
+}
---
+++ src/template.R
@@ -0,0 +1,145 @@
+# --- (BOBA_CONFIG)
+{
+  "decisions": [
+    {"var": "filter", "options": [
+      "Female == 1",
+      "UniqueFemaleContributors >= 1 & DebateSize > 1",
+      "Type == 2"
+    ]},
+    {"var": "DV", "options": [
+      "ContributionsbyAuthor",
+      "FemaleParticipation",
+      "WC",
+      "CommentsChange"
+    ]},
+    {"var": "IV", "options": [
+      "UniqueFemaleContributors",
+      "FemaleCumulativeProportion"
+    ]},
+    {"var": "covariates", "options": [
+      "+ PreviousContributions + HavePhD + Total_Citations",
+      "+ UniqueContributors",
+      ""
+    ]}
+  ],
+  "constraints": [
+    {"variable": "DV", "option": "ContributionsbyAuthor", 
+      "condition": "filter.index == 0 and Unit != thread"},
+    {"variable": "DV", "option": "FemaleParticipation",
+      "condition": "Unit != comment"},
+    {"variable": "DV", "option": "CommentsChange", 
+      "condition": "filter.index == 0"},
+    {"variable": "DV", "option": "WC", 
+      "condition": "filter.index == 0"},
+    {"variable": "IV", "option": "FemaleCumulativeProportion",
+      "condition": "Unit == comment"},
+    {"variable": "covariates", "index": 0,
+      "condition": "Unit != thread"}
+  ],
+  "before_execute": "rm -rf results && mkdir results"
+}
+# --- (END)
+
+library(readr)
+library(tidyverse)
+library(broom.mixed)
+source('../../boba_util.R')
+
+df <- read.csv(file='../../../data/edge1.1_anonymized.csv')
+
+# augment the dataset with additional variables
+# cumulative proportion of females in each conversation
+df <- df %>% 
+  group_by(ThreadId) %>% 
+  mutate(FemaleCumulativeProportion = cummean(Female) * 100) %>% 
+  ungroup
+
+# difference between female comments in current conversation and previous conversation
+tmp <- df %>%
+  group_by(ThreadId, Id) %>% 
+  summarise(comments_now = n(),
+            PreviousContributions = mean(PreviousContributions),
+            PreviousThreads = mean(PreviousThreads),
+            ContributionsThisYear = mean(ContributionsThisYear),
+            ThreadsThisYear = mean(ThreadsThisYear)) %>%
+  mutate(cont_per_thread = (PreviousContributions + ContributionsThisYear)/(PreviousThreads + ThreadsThisYear),
+         CommentsChange = (comments_now - cont_per_thread)*100/cont_per_thread) %>%
+  select(CommentsChange, ThreadId, Id)
+
+df = left_join(df, tmp, by=c('Id' = 'Id', 'ThreadId' = 'ThreadId'))
+
+# filtering
+df <- df %>%
+  filter({{filter}})
+
+# todo: remove the outlier in A6?
+
+# --- (Unit) comment
+
+# --- (Unit) thread
+# first perform necessary per-thread aggregation, then keep one row per thread
+df <- df %>%
+  group_by(ThreadId) %>%
+  mutate(
+    CommentsChange = mean(CommentsChange),
+    WC = mean(WC)
+  ) %>%
+  ungroup %>%
+  distinct(ThreadId, .keep_all = TRUE)
+
+# --- (Unit) author
+df <- df %>%
+  mutate(
+    WC = mean(WC)
+  ) %>%
+  ungroup %>%
+  distinct(ThreadId, Id, .keep_all = TRUE)
+
+# # --- (Unit) custom_A2
+# # calculate the DV and IV for A2
+# # number of female contributors ordered by time of commenting (first, second, third female contributor, etc)
+# df <- df %>%
+#   filter(Female == 1) %>%
+#   arrange(ThreadId, Id_num, Order) %>% 
+#   group_by(ThreadId, Id_num) %>%
+#   mutate(first_post = c(1, rep(0,n()-1))) %>% # mark 1st post 1, others 0
+#   ungroup() %>%
+#   arrange(ThreadId, Order) %>% 
+#   group_by(ThreadId) %>% 
+#   mutate(n_females = lag(cumsum(first_post)),
+#          n_females = ifelse(is.na(n_females), 0, n_females)
+#   ) %>%
+#   ungroup() %>%
+#   group_by(ThreadId, n_females) %>%
+#   summarise(n_posts= n()) %>%
+#   ungroup()
+
+# --- (Model) lm
+model <- lm({{DV}} ~ {{IV}} {{covariates}}, data=df)
+summary(model)
+
+# --- (O)
+# cross validation
+fit <- cross_validation(df, model, '{{DV}}')
+nrmse = fit / (max(df${{DV}}) - min(df${{DV}}))
+
+# wrangle results
+result <- tidy(model, conf.int = TRUE) %>%
+  filter(term == '{{IV}}') %>%
+  add_column(NRMSE = nrmse)
+
+# get predictions
+disagg_fit <- pointwise_predict(model, df) %>%
+  select(
+    observed = '{{DV}}',
+    expected = fit
+  )
+
+# get uncertainty
+uncertainty <- sampling_distribution(model, '{{IV}}') %>%
+  select(estimate = coef)
+
+# output
+write_csv(result, '../results/estimate_{{_n}}.csv')
+write_csv(disagg_fit, '../results/disagg_fit_{{_n}}.csv')
+write_csv(uncertainty, '../results/uncertainty_{{_n}}.csv')
