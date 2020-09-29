68310c473a14232c37db5a32f9fe802a25599b84
Yang Liu <yliu0@cs.washington.edu>
2020-09-28 18:30:36 -0700

    Refactor

--- src/find_taken_paths.py
+++ src/find_taken_paths.py
@@ -1,21 +1,29 @@
 # find the universe # corresponding to paths taken by analysts
 import pandas as pd
 
-taken = pd.read_csv('analysts.csv', dtype=str, na_filter=False)
+def find_taken_paths (fn='multiverse/summary.csv'):
+  taken = pd.read_csv('analysts.csv', dtype=str, na_filter=False)
 
-df = pd.read_csv('multiverse/summary.csv', dtype=str, na_filter=False)
+  df = pd.read_csv(fn, dtype=str, na_filter=False)
 
-for idx, row in taken.iterrows():
-  res = df.loc[
-    (df['Unit'] == row['Unit']) &
-    (df['Model'] == row['Model']) &
-    (df['filter'] == row['filter']) &
-    (df['DV'] == row['DV']) &
-    (df['IV'] == row['IV']) &
-    (df['covariates'] == row['covariates']) &
-    (df['random_term'] == row['random_term'])
-  ]
-  if res.shape[0] > 0:
-    print(f'{row.analyst} is {res.Filename.iloc[0]}')
-  else:
-    print(f'Cannot find {row.analyst}')
+  found = []
+  for idx, row in taken.iterrows():
+    res = df.loc[
+      (df['Unit'] == row['Unit']) &
+      (df['Model'] == row['Model']) &
+      (df['filter'] == row['filter']) &
+      (df['DV'] == row['DV']) &
+      (df['IV'] == row['IV']) &
+      (df['covariates'] == row['covariates']) &
+      (df['random_term'] == row['random_term'])
+    ]
+    uid = res.Filename.iloc[0] if res.shape[0] > 0 else ''
+    found.append({'analyst': row.analyst, 'uid': uid})
+
+  return found
+
+
+if __name__ == '__main__':
+  res = find_taken_paths()
+  for r in res:
+    print('{} is {}'.format(r['analyst'], r['uid']))
